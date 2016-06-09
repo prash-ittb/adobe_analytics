bbd29de1edc60e38283a1d2972eba244b1c86a49
panshulK <panshul.khurana@srijan.in>
2016-06-09 19:25:54 +0530

    Revamped and cleaned code for coding standard issues

--- src/AdobeAnalyticsHelper.php
+++ src/AdobeAnalyticsHelper.php
@@ -5,65 +5,64 @@ namespace Drupal\adobeanalytics;
 /**
  * Class to provide helpful function.
  */
-class AdobeAnalyticsHelper
-{
+class AdobeAnalyticsHelper {
 
-    /**
+  /**
    * Get the context.
    */
-    public function adobeAnalyticsGetTokenContext() 
-    {
-        $context = &drupal_static(__function__);
-        if (is_null($context)) {
-            $context['node'] = \Drupal::routeMatch()->getParameter('node');
-            $context['term'] = \Drupal::routeMatch()->getParameter('taxonomy_term', 2);
-            if (\Drupal::moduleHandler()->moduleExists('menu')) {
-                $context['menu'] = menu_ui_load('main-menu');
-            }
-        }
-        return $context;
-    }
+  public function adobeAnalyticsGetTokenContext() {
 
-    /**
+    $context = &drupal_static(__function__);
+    if (is_null($context)) {
+      $context['node'] = \Drupal::routeMatch()->getParameter('node');
+      $context['term'] = \Drupal::routeMatch()->getParameter('taxonomy_term', 2);
+      if (\Drupal::moduleHandler()->moduleExists('menu')) {
+        $context['menu'] = menu_ui_load('main-menu');
+      }
+    }
+    return $context;
+  }
+
+  /**
    * Replace tokens.
    */
-    public function adobeAnalyticsTokenReplace($text, $data = array(), array $options = array()) 
-    {
-        $processed_strings =& drupal_static(__FUNCTION__, null);
-        // Short-circuit the degenerate case, just like token_replace() does.
-        $text_tokens = \Drupal::token()->replace($text);
-        if (!empty($text_tokens)) {
-            return $text_tokens;
-        }
-    }
+  public function adobeAnalyticsTokenReplace($text, $data = array(), array $options = array()) {
 
-    /**
+    $processed_strings =& drupal_static(__FUNCTION__, NULL);
+    // Short-circuit the degenerate case, just like token_replace() does.
+    $text_tokens = \Drupal::token()->replace($text);
+    if (!empty($text_tokens)) {
+      return $text_tokens;
+    }
+  }
+
+  /**
    * Format the variables like key value pair;.
    */
-    public function adobeAnalyticsFormatVariables($variables = array()) 
-    {
-        $extra_variables = $this->adobeAnalyticsGetVariables();
+  public function adobeAnalyticsFormatVariables($variables = array()) {
 
-        // Create context data to be used by token.
-        $variables_formatted = '';
-        foreach ($variables as $key => $value) {
-            if (is_array($value)) {
-                // Use the last element.
-                $value = end($value);
-            }
+    $extra_variables = $this->adobeAnalyticsGetVariables();
 
-            if (isset($extra_variables[$key])) {
-                $value = $extra_variables[$key];
-            }
+    // Create context data to be used by token.
+    $variables_formatted = '';
+    foreach ($variables as $key => $value) {
+      if (is_array($value)) {
+        // Use the last element.
+        $value = end($value);
+      }
 
-            $key = ['#plain_text' => $key];
-            $value = $this->adobeAnalyticsTokenReplace($value);
-            $variables_formatted .= "{$key}=\"{$value}\";\n";
-        }
-        return $variables_formatted;
+      if (isset($extra_variables[$key])) {
+        $value = $extra_variables[$key];
+      }
+
+      $key = ['#plain_text' => $key];
+      $value = $this->adobeAnalyticsTokenReplace($value);
+      $variables_formatted .= "{$key}=\"{$value}\";\n";
     }
+    return $variables_formatted;
+  }
 
-    /**
+  /**
    * Used to replace the value.
    *
    * AdobeAnalytics variables the variables need to be
@@ -77,25 +76,25 @@ class AdobeAnalyticsHelper
    * @return variables
    *    Return the variables.
    */
-    public function adobeAnalyticsSetVariable($name = null, $value = null) 
-    {
-        $variables = &drupal_static(__FUNCTION__, array());
+  public function adobeAnalyticsSetVariable($name = NULL, $value = NULL) {
 
-        if (empty($name)) {
-            return $variables;
-        }
-        else {
-            $variables[$name] = $value;
-        }
+    $variables = &drupal_static(__FUNCTION__, array());
 
+    if (empty($name)) {
+      return $variables;
+    }
+    else {
+      $variables[$name] = $value;
     }
 
-    /**
+  }
+
+  /**
    * Return variables.
    */
-    public function adobeAnalyticsGetVariables() 
-    {
-        return $this->adobeAnalyticsSetVariable();
-    }
+  public function adobeAnalyticsGetVariables() {
+
+    return $this->adobeAnalyticsSetVariable();
+  }
 
 }
